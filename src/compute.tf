051bc825ef140443b551ce2ff50cd0798e46d18a
Hiroshi Hayakawa <hhiroshell@gmail.com>
2020-09-16 01:18:11 +0900

    use gatling on docker

--- src/compute.tf
+++ src/compute.tf
@@ -15,25 +15,32 @@
 locals {
     instance-image-ocid = {
         // See https://docs.us-phoenix-1.oraclecloud.com/images/
-        // Oracle-provided image "Oracle-Linux-6.10-2019.08.02-0"
-        ap-mumbai-1    = "ocid1.image.oc1.ap-mumbai-1.aaaaaaaae7wf2oyhqq45oius5y7lzbs4vgbioaue24rzzayjfdmhv3jkx2xq"
-        ap-seoul-1     = "ocid1.image.oc1.ap-seoul-1.aaaaaaaapwlyu3dtjeskcchf7f5cbm54ghi426htb6lvkgzrbehgkzwgq7aq"
-        ap-tokyo-1     = "ocid1.image.oc1.ap-tokyo-1.aaaaaaaarwx3indflmgjwdgeoxge3vsasdbkdowsrxb5p6g7k635vwmqun3a"
-        ca-toronto-1   = "ocid1.image.oc1.ca-toronto-1.aaaaaaaaz6jvtjknnxhkzk3cja2uear5vgmwmubeur2dhj6ofijbatyipvaq"
-        eu-frankfurt-1 = "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaas5n4xc7zfrzg42pq7yvlkhimmsug4p27uylpgi2l5ja2o3hhcyuq"
-        eu-zurich-1    = "ocid1.image.oc1.eu-zurich-1.aaaaaaaarztkkowway3ec7lk5h2riuis6a3d2f6qwgrdneeczbawuog47fna"
-        uk-london-1    = "ocid1.image.oc1.uk-london-1.aaaaaaaauyqawuj67nro2s3ej6dce2qrthfx5iats3m7pclqcvh3d2y35nma"
-        us-ashburn-1   = "ocid1.image.oc1.iad.aaaaaaaaqaedrqy4ugykvsp25nfx5ozotnmgs4fdgc65rmq4aru7mss4ga3a"
-        us-langley-1   = "ocid1.image.oc2.us-langley-1.aaaaaaaaqhnu5vu5wod452kwk7invmhf3xwcylkutyqana6msps6fnoxrdjq"
-        us-luke-1      = "ocid1.image.oc2.us-luke-1.aaaaaaaa2axsqcwjwpb377i4uoif3gw4rbbmrmjdsgoditazcvuy6t3vxp4a"
-        us-phoenix-1   = "ocid1.image.oc1.phx.aaaaaaaaoxrc5doxp7zoi7w6z6td6sqxjwzs2criwblz2arstmtjuy53smta"
+        // Oracle-provided image "Canonical-Ubuntu-18.04-Minimal-2020.08.25-0"
+        ap-chuncheon-1 = "ocid1.image.oc1.ap-chuncheon-1.aaaaaaaa4f3s4zgbvtwcyxv4a4ghbe224b25pgm6xht7fbevjhl5at4mpida"
+        ap-hyderabad-1 = "ocid1.image.oc1.ap-hyderabad-1.aaaaaaaawjc3lipandj7lxylfbrka7p6zc2nj3gzqqsqou2potgoxk76wkfq"
+        ap-melbourne-1 = "ocid1.image.oc1.ap-melbourne-1.aaaaaaaaxh6rqavgy6zlwao64ghuhuchftad4f6s7y5kk4jcou6z62lf2rma"
+        ap-mumbai-1 = "ocid1.image.oc1.ap-mumbai-1.aaaaaaaa7lbnymwk2cki7qdywnjvxakliw6xjtrv7od5tti2vwmtk3lzvk2a"
+        ap-osaka-1 = "ocid1.image.oc1.ap-osaka-1.aaaaaaaapcnrwzsfmy37alrkcv2j4jkuyl6jrc4jz66pru5xf6eaid2rk7va"
+        ap-seoul-1 = "ocid1.image.oc1.ap-seoul-1.aaaaaaaaoa7r3yfuurfnqhfltd453p2yb7pll63lfp5qzw7ijmptrhjbvz3a"
+        ap-sydney-1 = "ocid1.image.oc1.ap-sydney-1.aaaaaaaaoleh53spl5cvrvb54syl6nxy4md77pupkrehtc5okfgrfievgrcq"
+        ap-tokyo-1 = "ocid1.image.oc1.ap-tokyo-1.aaaaaaaa27lu2aswvgwc7vd7l6gwrnagxlvjzybq5eqedrg6klmaqykoaaba"
+        ca-montreal-1 = "ocid1.image.oc1.ca-montreal-1.aaaaaaaa5orh3fe5scqkwnpjd4ak2glhzixiq5v4mqhd4h6lic25labhwppq"
+        ca-toronto-1 = "ocid1.image.oc1.ca-toronto-1.aaaaaaaarg7zoxcn63kztvglt3l6kfdxnr4ipmguzauzyaopwj7uutqvskvq"
+        eu-amsterdam-1 = "ocid1.image.oc1.eu-amsterdam-1.aaaaaaaabura3z6avfytshsbymfcflgr75ohgotqwrrbne5o5oqmagrn6noa"
+        eu-frankfurt-1 = "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaajag7koakq7h6nhwofyzalrctekkxa4lsnvtgb56adzfegod5dmpa"
+        eu-zurich-1 = "ocid1.image.oc1.eu-zurich-1.aaaaaaaaj4wfdbm5szmfwj52uzdgg4k7zcnllfvv5rf7x4dcyvib5wv2b7vq"
+        me-jeddah-1 = "ocid1.image.oc1.me-jeddah-1.aaaaaaaa576wmmzbbtxcb5sotrx4yabie6kaeiyka5pkbjnrvogvvoaxgtsa"
+        sa-saopaulo-1 = "ocid1.image.oc1.sa-saopaulo-1.aaaaaaaajvvn6z6o7gbmhaekz4dvadaxhiqs2kjoqstumiqzm4vfmbex4rca"
+        uk-london-1 = "ocid1.image.oc1.uk-london-1.aaaaaaaabdbcjkzxbuqordrm7gtsopgkljindcebwalhocziprr7h4xzjplq"
+        us-ashburn-1 = "ocid1.image.oc1.iad.aaaaaaaa5cr5r2tsyurtz5hh2sb3cq4qrda4tsll2dwp22snwqwi2qzlvhtq"
+        us-gov-ashburn-1 = "ocid1.image.oc3.us-gov-ashburn-1.aaaaaaaanjsc6ey3hwaifs43kyhwu7typzx5qibcc74cbw6afws5wpf4falq"
+        us-gov-chicago-1 = "ocid1.image.oc3.us-gov-chicago-1.aaaaaaaajy3embkfmrdedhf3ielyqee5bcbelgw4ogoeuup43v5vgs4vshpa"
+        us-gov-phoenix-1 = "ocid1.image.oc3.us-gov-phoenix-1.aaaaaaaahwxzh5fnqv74lpuaw7nx7edwqysxibi7bcfjyflyemg3nhr3b5vq"
+        us-langley-1 = "ocid1.image.oc2.us-langley-1.aaaaaaaacclsdxcp6p2qugzjzrvvta335e32khrttf6j73jkgbpfun3igmja"
+        us-luke-1 = "ocid1.image.oc2.us-luke-1.aaaaaaaawjrjk66dn6p7ykkebyi6peznqcjk4y6des7tkcpzutyqnbcmvafq"
+        us-phoenix-1 = "ocid1.image.oc1.phx.aaaaaaaasit6dalhenmeijklokta5qg2ga62ycuxkugjpwwep6lsvjrycunq"
+        us-sanjose-1 = "ocid1.image.oc1.us-sanjose-1.aaaaaaaahh3yy7byc2zndii7zo74x2lbr6tcqbqmcmkwzfnicnapg4wfxibq"
     }
-
-    gatling-version = "3.2.0"
-    gatling-archive = "gatling-charts-highcharts-bundle-${local.gatling-version}-bundle.zip"
-    gatling-base = "/home/opc/gatling"
-    gatling-home = "${local.gatling-base}/gatling-charts-highcharts-bundle-${local.gatling-version}"
-
 }
 
 resource "tls_private_key" "loadtester-ssh-key-pair" {
@@ -67,41 +74,26 @@ resource "oci_core_instance" "loadtester" {
         private_key = "${tls_private_key.loadtester-ssh-key-pair.private_key_pem}"
     }
 
-    provisioner "remote-exec" "install jdk & gatling" {
+    provisioner "remote-exec" "install dev instruments" {
         inline = [
-            "sudo yum -y update",
-            "sudo yum -y install java-1.8.0-openjdk",
-            "wget https://repo1.maven.org/maven2/io/gatling/highcharts/gatling-charts-highcharts-bundle/${local.gatling-version}/${local.gatling-archive} -O /tmp/${local.gatling-archive}",
-            "mkdir ~/gatling",
-            "unzip /tmp/${local.gatling-archive} -d ${local.gatling-base}",
+            "sudo apt-get update",
+            "sudo apt-get install -y vim git",
         ]
     }
 
-    provisioner "remote-exec" "preparation for copying files" {
+    provisioner "remote-exec" "install docker & docker-compose" {
         inline = [
-            "mkdir ${local.gatling-home}/user-files/simulations/atpstore",
+            # docker
+            "sudo apt-get update",
+            "sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common",
+            "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -",
+            "sudo add-apt-repository \"deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable\"",
+            "sudo apt-get update",
+            "sudo apt-get install -y docker-ce docker-ce-cli containerd.io",
+            # docker-compose,
+            "sudo curl -L \"https://github.com/docker/compose/releases/download/1.27.1/docker-compose-$(uname -s)-$(uname -m)\" -o /usr/local/bin/docker-compose",
+            "sudo chmod +x /usr/local/bin/docker-compose",
         ]
-    }
-
-    provisioner "file" {
-        source = "./gatling-home/simulations/atpstore/atpstoreRead.scala"
-        destination = "${local.gatling-home}/user-files/simulations/atpstore/atpstoreRead.scala"
-    }
-
-    provisioner "file" {
-        source = "./gatling-home/run.sh"
-        destination = "${local.gatling-home}/run.sh"
-    }
-
-    provisioner "remote-exec" "change file permission" {
-        inline = [
-            "chmod +x ${local.gatling-home}/run.sh",
-        ]
-    }
-
-    provisioner "file" {
-        source = "./gatling-home/endpoints.txt"
-        destination = "${local.gatling-home}/endpoints.txt"
     }
 
     /* 
@@ -113,6 +105,7 @@ resource "oci_core_instance" "loadtester" {
             "sudo bash -c 'echo \"*       soft    nofile  65535\" >> /etc/security/limits.conf'",
             "sudo bash -c 'echo \"*       hard    nofile  65535\" >> /etc/security/limits.conf'",
             "sudo bash -c 'echo \"session required pam_limits.so\" >> /etc/pam.d/common-session'",
+            "sudo bash -c 'echo \"session required pam_limits.so\" >> /etc/pam.d/common-session-noninteractive'",
             "sudo bash -c 'echo \"session required pam_limits.so\" >> /etc/pam.d/sshd'",
             "sudo bash -c 'echo \"UseLogin yes\" >> /etc/ssh/sshd_config'",
             "sudo sysctl -w net.ipv4.ip_local_port_range=\"1025 65535\"",
